fea40ae24e4f270df4181c84cb6bdc399b6c6e0b
Vaish123-M <vaishmamulkar@gmail.in>
2025-07-26 19:14:55 +0530

    Creted abhyudaya page in src and done with the folder structure as it was required in vaishnavi branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+// src/App.js
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
+import AbhyudayaPage from "./pages/Abhyudaya";
 
 function App() {
   return (
     <div className="w-full">
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/abhyudaya" element={<AbhyudayaPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
