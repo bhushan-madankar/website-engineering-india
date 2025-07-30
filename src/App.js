79bd502da9a59a1fef65002ad70d8deca3929181
Vaish123-M <vaishmamulkar@gmail.in>
2025-07-30 12:51:42 +0530

    Worked on the UI of abhyudaya page in vaishnavi branch

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
- homepg-2
-import React from "react";
-import HomePage from "./pages/Home/Homepg";
-import ClgSec from "./pages/ClgSec/ClgSec";
-import EventsPage from "./components/Eventpage";
-
-function App() {
-  return (
-    <div className="w-full">
-      <HomePage />
-      <EventsPage />
-      <ClgSec />
-    </div>
-
-// App.js
-
-import React from 'react';
+ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import AbhyudayaPage from './pages/Abhyudaya'; // index.js gets auto-resolved
+import HomePage from './pages/Home/Homepg';
+import ClgSec from './pages/ClgSec/ClgSec';
+import EventsPage from './components/Eventpage';
+import AbhyudayaPage from './pages/Abhyudaya';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Redirect root to /abhyudaya */}
         <Route path="/" element={<Navigate to="/abhyudaya" />} />
         <Route path="/abhyudaya" element={<AbhyudayaPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/college" element={<ClgSec />} />
+        <Route path="/events" element={<EventsPage />} />
       </Routes>
     </Router>
- main
   );
 }
 
