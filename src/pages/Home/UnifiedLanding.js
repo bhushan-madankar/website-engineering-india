38196e061f6b4d037ad5f21bfc1d73bec0fd50f2
Vaish123-M <vaishmamulkar@gmail.in>
2025-07-30 16:55:21 +0530

    Fixed UnifiedLanding to render all pages together

--- src/pages/Home/UnifiedLanding.js
+++ src/pages/Home/UnifiedLanding.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './Homepg';
+import ClgSec from '../ClgSec/ClgSec';
+import EventsPage from '../../components/Eventpage';
+import AbhyudayaPage from '../Abhyudaya/Abyudaya';
+import MagazinePage from '../Magazine';
 
-import UnifiedLanding from './pages/Home/UnifiedLanding';
-import ClgSec from './pages/ClgSec/ClgSec';
-import EventsPage from './components/Eventpage';
-import AbhyudayaPage from './pages/Abhyudaya/Abyudaya';
-import MagazinePage from './pages/Magazine';
-
-function App() {
+const UnifiedLanding = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<UnifiedLanding />} />
-        <Route path="/college" element={<ClgSec />} />
-        <Route path="/events" element={<EventsPage />} />
-        <Route path="/abhyudaya" element={<AbhyudayaPage />} />
-        <Route path="/magazine" element={<MagazinePage />} />
-      </Routes>
-    </Router>
+    <div className="space-y-12 px-4">
+      <HomePage />
+      <ClgSec />
+      <EventsPage />
+      <AbhyudayaPage />
+      <MagazinePage />
+    </div>
   );
-}
+};
 
-export default App;
+export default UnifiedLanding;
